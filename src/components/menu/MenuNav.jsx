2e13917f9599b15b7a5747fe1ce80ee708832a88
Pankaj Kumar <142082497+kumarpankaj3404@users.noreply.github.com>
2025-08-24 19:40:18 +0530

    Worked on menuPage with localData

--- src/components/menu/MenuNav.jsx
+++ src/components/menu/MenuNav.jsx
@@ -18,16 +18,16 @@ return (
         <div className='w-full h-20 bg-[#8a5353c4] flex items-center justify-center font-cursive text-2xl font-bold text-white'>
         </div>
 
-        <div className='flex justify-center items-center bg-[#f5f5f5] dark:bg-gray-800 py-4 border-b-2 border-gray-300 dark:border-gray-700 mb-4 p-4' >
+        <div className='flex gap-8  justify-center items-center bg-[#f5f5f5] dark:bg-gray-800 py-4 border-b-2 border-gray-300 dark:border-gray-700 mb-4 p-4' >
             {categories.map((category, index) => (
                 <Link
                     key={index}
                     to={`/menu/${category.link}`}
-                    className={`m-4 p-2 text-lg font-medium text-gray-700  dark:text-gray-300  ${
+                    className={` p-2 text-lg font-light text-gray-700  dark:text-gray-300  ${
                         props.onPage === category.link
                             ? 'border-b-2 border-green-500 text-red-600 dark:text-yellow-300'
                             : 'hover:text-[#a37565] dark:hover:text-[#86C232] '
-                    } transition-colors duration-300`}
+                    } transition-colors  duration-300`}
                 >
                     {category.name}
                 </Link>
--- src/main.jsx
+++ src/main.jsx
@@ -14,13 +14,16 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
-import { store } from './app/store.js';
+import { store, persistor } from './app/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 import ThemeProvider from './app/ThemeProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </PersistGate>
   </Provider>
 );
