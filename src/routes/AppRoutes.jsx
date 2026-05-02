5dd449d1729bedbf1cb7b39f151b848c8fbefb6b
NikitaBharti27 <nikitabharti2116@gmail.com>
2026-05-02 21:14:22 +0530

    Resolved merge conflicts

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,42 +1,4 @@
 
-// import React from "react";
-// import { Routes, Route, Navigate } from "react-router-dom";
-// import Home from "../pages/Home";
-// import Reservation from "../pages/Reservation";
-// import Cart from "../pages/Cart";
-// import Gallery from "../pages/Gallery";
-// import Menu from "../pages/Menu";
-// import About from "../pages/About";
-
-// import Avtar from "../components/profile/avtar";
-// import AdminDashboard from "../pages/admin/AdminDashboard";
-
-// export default function AppRoutes({ onLoginClick }) {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Home />} />
-//       <Route
-//       path="/reservation"
-//       element={<Reservation onLoginClick={onLoginClick} />}/>
-      
-      
-//       <Route path="/menu" element={<Navigate to="/menu/starters-snacks" />} />
-//       <Route path="/menu/:categories" element={<Menu />} />
-
-      
-//       <Route path="/gallery" element={<Navigate to="/gallery/all" />} />
-//       <Route path="/gallery/:category" element={<Gallery />} />
-
-//       <Route path="/cart" element={<Cart />} />
-//       <Route path="/about" element={<About/>}/>
-
-//       <Route path="/profile" element={<Avtar onLoginClick={onLoginClick} />}/>
-//       <Route path="/admindashboard" element={<AdminDashboard/>}/>
-      
-//     </Routes>
-//   );
-// }
-
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
@@ -59,6 +21,7 @@ import AdminDashboard from "../pages/admin/AdminDashboard";
 export default function AppRoutes({ onLoginClick }) {
   return (
     <Routes>
+
 
       {/* ✅ MAIN LAYOUT (Navbar + Footer) */}
       <Route element={<MainLayout onLoginClick={onLoginClick} />}>
